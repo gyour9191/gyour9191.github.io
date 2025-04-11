5f1d82cb3ef25ed2f6989f49ad80518b22ce26af
Gyou_R <159975999+gyour9191@users.noreply.github.com>
2025-04-12 02:20:46 +0900

    Add files via upload

---
+++ event.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("yt").addEventListener("click", function () {
+      window.open("https://www.youtube.com/@gyoul9191", "_blank");
+    });
+  
+    document.getElementById("dc").addEventListener("click", function () {
+      window.open("https://discord.gg/jsvugypq2w", "_blank");
+    });
+  
+    document.getElementById("x").addEventListener("click", function () {
+      window.open("https://x.com/gyoul9191", "_blank");
+    });
+  
+    document.getElementById("gh").addEventListener("click", function () {
+      window.open("https://github.com/gyour9191", "_blank");
+    });
+  
+    document.getElementById("qv").addEventListener("click", function () {
+      window.open("https://quavergame.com/user/581257", "_blank");
+    });
+  });
